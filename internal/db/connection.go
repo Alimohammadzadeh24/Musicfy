ca6aee94ffc2bda60be81e4b321b667c3ace428a
Alimmz <ali.mo@bit24.info>
2025-07-14 15:12:14 +0330

    Add .env file with database configuration and JWT secret for environment management

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -23,15 +22,8 @@ func InitializeDatabase() {
 			log.Fatalf("Error loading .env file: %v", err)
 		}
 
-		host := os.Getenv("DB_HOST")
-		port := os.Getenv("DB_PORT")
-		user := os.Getenv("DB_USER")
-		password := os.Getenv("DB_PASSWORD")
-		dbname := os.Getenv("DB_NAME")
-		sslmode := os.Getenv("DB_SSLMODE")
-
-		dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
-		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		dbUrl := os.Getenv("DATABASE_URL")
+		db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
 		if err != nil {
 			log.Fatalf("Failed to connect to database: %v", err)
 		}
